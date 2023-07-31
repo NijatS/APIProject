8445e8aa778d87833c0bf8838cb0badcb294b933
NijatS <nijatjs@code.edu.az>
2023-07-31 16:10:50 +0400

    add:RepositoryInterfaces

---
+++ APIProject.Core/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using APIProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProject.Core.Repositories
+{
+    public interface IBookRepository:IRepository<Book>
+    {
+    }
+}
