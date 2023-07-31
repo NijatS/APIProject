763bc15ef7ad3b8736be435d8f10a96ed2374b97
NijatS <nijatjs@code.edu.az>
2023-07-31 18:28:15 +0400

    add:CategoryController

---
+++ APIProject.Service/Services/Implementations/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using APIProject.Service.Dtos.Categories;
+using APIProject.Service.Responses;
+using APIProject.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProject.Service.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<ApiResponse> CreateAsync(CategoryPostDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> UpdateAsync(int id, CategoryUpdateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- APIProject.Service/Services/Interfaces/ICategoryService.cs
+++ APIProject.Service/Services/Interfaces/ICategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using APIProject.Service.Dtos.Categories;
+using APIProject.Service.Responses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace APIProject.Service.Services.Interfaces
 {
     public interface ICategoryService
     {
-        public 
+        public Task<ApiResponse> CreateAsync(CategoryPostDto dto);
+        public Task<ApiResponse> GetAsync(int id);
+        public Task<ApiResponse> GetAllAsync();
+        public Task<ApiResponse> UpdateAsync(int id, CategoryUpdateDto dto);
+        public Task<ApiResponse> RemoveAsync(int id);
     }
 }
