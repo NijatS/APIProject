3c71b724ee90dd63c3894683af58cd58b33f9212
NijatS <nijatjs@code.edu.az>
2023-07-31 16:04:38 +0400

    add:Book&&CategoryTabletoDB

--- APIProject.App/Program.cs
+++ APIProject.App/Program.cs
@@ -1,3 +1,6 @@
+using APIProject.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<APIProjectDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
